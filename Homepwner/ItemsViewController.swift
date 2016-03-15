14e97332eb97ef6a596f18c580b33c421869c7fe
jennifertigner <jennifertigner@gmail.com>
2016-03-15 16:12:38 -0700

    Added ItemsViewController file and assigned it a table view controller

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Jennifer Tigner on 2016-03-15.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
