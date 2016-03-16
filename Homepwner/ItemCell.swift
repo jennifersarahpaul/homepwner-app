4eac46ffe9fcdace148e562f5e6a7f33cff765c7
jennifertigner <jennifertigner@gmail.com>
2016-03-16 12:13:24 -0700

    Populated labels with name, serial number, and price

--- Homepwner/ItemCell.swift
+++ Homepwner/ItemCell.swift
@@ -10,4 +10,8 @@ import UIKit
 
 class ItemCell: UITableViewCell {
     
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
 }
