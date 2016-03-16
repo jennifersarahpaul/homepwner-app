44b0dd88c7d3843fe3fd11136fe8e469b4244dae
jennifertigner <jennifertigner@gmail.com>
2016-03-16 11:51:26 -0700

    Created Item Cell subclass

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jennifer Tigner on 2016-03-16.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
